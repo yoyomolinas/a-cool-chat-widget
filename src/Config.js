881ac9ca9cdd6ee1944e0b62d79ed45e7a66b44f
Yoel <molinas.yoel@gmail.com>
2021-01-17 22:37:14 +0000

    build

--- src/Config.js
+++ src/Config.js
@@ -3,6 +3,5 @@ export default {
     publishKey: "pub-c-41a236a2-8f41-4e31-93d7-1c5066f3f645",
     subscribeKey: "sub-c-73a39e08-5226-11eb-9d3f-7e8713e36938",
   },
-
   elementId: "the-best-chat-widget-in-the-world", // element id in html
 };
